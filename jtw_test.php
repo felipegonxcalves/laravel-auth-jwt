d47a2c598a16e5176f94a0eee9ce9232e0217130
felipeconxceicao <felipe.conceicao@infocast.com.br>
2020-08-31 16:25:58 -0300

    first commit

---
+++ jtw_test.php
@@ -0,0 +1,47 @@
+<?php
+// header - payload (carga de dados - corpo de dados) - signature
+
+//header
+$header = [
+    'alg' => 'HS256', // HMAC - SHA256
+    'typ' => 'JWT'
+];
+
+$header_json = json_encode($header);
+$header_base64 = base64_encode($header_json);
+
+echo "Cabecalho JSON: $header_json";
+echo "\n";
+echo "Cabecalho JWT: $header_base64";
+
+$payload = [
+    'first_name' => 'Felipe',
+    'last_name' => 'Gonçalves',
+    'email' => 'felipeconxceicao@gmail.com',
+    'exp' => (new \DateTime())->getTimestamp()
+];
+
+echo "\n\n";
+
+$payload_json = json_encode($payload);
+$payload_base64 = base64_encode($payload_json);
+echo "Payload JSON: $payload_json";
+echo "\n";
+echo "Payload JWT: $payload_base64";
+
+$key = '8484rf8r4g848f4g8f484sdsc8xcv4t8hb4g84t8';
+
+echo "\n\n";
+
+$signature = hash_hmac('sha256', "$header_base64.$payload_base64", $key, true);
+$signature_base64 = base64_encode($signature);
+
+echo "Signature war: $signature";
+echo "\n";
+echo "Signature JWT: $signature_base64";
+
+$token = "$header_base64.$payload_base64.$signature_base64";
+echo "\n\n";
+echo "TOKEN: $token";
+
+
